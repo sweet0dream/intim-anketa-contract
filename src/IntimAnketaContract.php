aba031c1dcc8ea01394825a478d02194c36b3e59
sweet0dream <sweet0dream@proton.me>
2023-12-01 12:35:04 +0300

    private $type from AnketaContract

--- src/IntimAnketaContract.php
+++ src/IntimAnketaContract.php
@@ -7,16 +7,25 @@ use Sweet0dream\PriceContract;
 
 class IntimAnketaContract {
 
+    private string $type;
+
+    public function __construct(
+        string $type
+    )
+    {
+        $this->type = $type;
+    }
+
     private const FIELD_DOP = [
         'text' => ['name' => 'Дополнительно о себе', 'type' => 'textarea', 'require' => 1]
     ];
     
-    public function getField(string $key): array
+    public function getField(): array
     {
         return [
-            'info' => (new InfoContract($key))->getData(),
+            'info' => (new InfoContract($this->type))->getData(),
             'service' => (new ServiceContract())->getData(),
-            'price' => (new PriceContract($key))->getData(),
+            'price' => (new PriceContract($this->type))->getData(),
             'dop' => self::FIELD_DOP
         ];
     }
