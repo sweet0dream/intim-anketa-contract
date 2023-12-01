87f6e62183ef3d8fa284e8b1646a3e662f12050c
sweet0dream <sweet0dream@proton.me>
2023-12-01 11:07:00 +0300

    Service in const

--- src/IntimAnketaContract.php
+++ src/IntimAnketaContract.php
@@ -4,6 +4,99 @@ namespace Sweet0dream;
 
 class IntimAnketaContract {
 
+    private const SERVICE_SEX = [
+        'name' => 'Секс',
+        'value' => [
+            'sk' => 'классический',
+            'sa' => 'анальный',
+            'sg' => 'групповой'
+        ]
+    ];
+
+    private const SERVICE_MIN = [
+        'name' => 'Минет',
+        'value' => [
+            'mp' => 'в презервативе',
+            'mb' => 'без презерватива',
+            'mg' => 'глубокий'
+        ]
+    ];
+
+    private const SERVICE_OKO = [
+        'name' => 'Окончание',
+        'value' => [
+            'og' => 'на грудь',
+            'ol' => 'на лицо',
+            'or' => 'в рот'
+        ]
+    ];
+
+    private const SERVICE_MAS = [
+        'name' => 'Массаж',
+        'value' => [
+            'mk' => 'классический',
+            'me' => 'эротический',
+            'mu' => 'урологический'
+        ]
+    ];
+
+    private const SERVICE_SAD = [
+        'name' => 'БДСМ',
+        'value' => [
+            'sp' => 'порка',
+            'ss' => 'страпон',
+            'sr' => 'госпожа/рабыня'
+        ]
+    ];
+
+    private const SERVICE_ZRE = [
+        'name' => 'Зрелища',
+        'value' => [
+            'zl' => 'лесби шоу',
+            'zs' => 'стриптиз',
+            'zi' => 'игрушки'
+        ]
+    ];
+
+    private const SERVICE_ORA = [
+        'name' => 'Оральные',
+        'value' => [
+            'ok' => 'кунилингус',
+            'oa' => 'анилингус',
+            'op' => 'поза 69'
+        ]
+    ];
+
+    private const SERVICE_IZV = [
+        'name' => 'Извращения',
+        'value' => [
+            'iz' => 'золотой дождь',
+            'ik' => 'копро',
+            'if' => 'фистинг'
+        ]
+    ];
+
+    private const SERVICE_PRO = [
+        'name' => 'Прочее',
+        'value' => [
+            'pe' => 'эскорт',
+            'ps' => 'с семейной парой',
+            'pv' => 'фото/видео съемка'
+        ]
+    ];
+
+    private const SERVICE = [
+        'sex' => self::SERVICE_SEX,
+        'min' => self::SERVICE_MIN,
+        'oko' => self::SERVICE_OKO,
+        'mas' => self::SERVICE_MAS,
+        'sad' => self::SERVICE_SAD,
+        'zre' => self::SERVICE_ZRE,
+        'ora' => self::SERVICE_ORA,
+        'izv' => self::SERVICE_IZV,
+        'pro' => self::SERVICE_PRO
+    ];
+
     function formatNum($number, $suffix): string
     {
         $keys = [2, 0, 1, 1, 1, 2];
@@ -16,7 +109,7 @@ class IntimAnketaContract {
     {
         return [
             'info' => $this->getInfo($key),
-            'service' => $this->getService(),
+            'service' => self::SERVICE,
             'price' => $this->getPrice($key),
             'dop' => [
                 'text' => ['name' => 'Дополнительно о себе', 'type' => 'textarea', 'require' => 1]
@@ -70,84 +163,6 @@ class IntimAnketaContract {
         }
 
         return $result ?? null;
-    }
-
-    private function getService(): array
-    {
-        return [
-            'sex' => [
-                'name' => 'Секс',
-                'value' => [
-                    'sk' => 'классический',
-                    'sa' => 'анальный',
-                    'sg' => 'групповой'
-                ]
-            ],
-            'min' => [
-                'name' => 'Минет',
-                'value' => [
-                    'mp' => 'в презервативе',
-                    'mb' => 'без презерватива',
-                    'mg' => 'глубокий'
-                ]
-            ],
-            'oko' => [
-                'name' => 'Окончание',
-                'value' => [
-                    'og' => 'на грудь',
-                    'ol' => 'на лицо',
-                    'or' => 'в рот'
-                ]
-            ],
-            'mas' => [
-                'name' => 'Массаж',
-                'value' => [
-                    'mk' => 'классический',
-                    'me' => 'эротический',
-                    'mu' => 'урологический'
-                ]
-            ],
-            'sad' => [
-                'name' => 'БДСМ',
-                'value' => [
-                    'sp' => 'порка',
-                    'ss' => 'страпон',
-                    'sr' => 'госпожа/рабыня'
-                ]
-            ],
-            'zre' => [
-                'name' => 'Зрелища',
-                'value' => [
-                    'zl' => 'лесби шоу',
-                    'zs' => 'стриптиз',
-                    'zi' => 'игрушки'
-                ]
-            ],
-            'ora' => [
-                'name' => 'Оральные',
-                'value' => [
-                    'ok' => 'кунилингус',
-                    'oa' => 'анилингус',
-                    'op' => 'поза 69'
-                ]
-            ],
-            'izv' => [
-                'name' => 'Извращения',
-                'value' => [
-                    'iz' => 'золотой дождь',
-                    'ik' => 'копро',
-                    'if' => 'фистинг'
-                ]
-            ],
-            'pro' => [
-                'name' => 'Прочее',
-                'value' => [
-                    'pe' => 'эскорт',
-                    'ps' => 'с семейной парой',
-                    'pv' => 'фото/видео съемка'
-                ]
-            ]
-        ];
     }
 
     private function getPrice(string $key): ?array
