9a450fd76d5829d988a97ff4aa166ec8243049c6
sweet0dream <sweet0dream@proton.me>
2023-12-01 12:25:59 +0300

    dop field in const

--- src/IntimAnketaContract.php
+++ src/IntimAnketaContract.php
@@ -6,6 +6,10 @@ use Sweet0dream\ServiceContract;
 use Sweet0dream\PriceContract;
 
 class IntimAnketaContract {
+
+    private const FIELD_DOP = [
+        'text' => ['name' => 'Дополнительно о себе', 'type' => 'textarea', 'require' => 1]
+    ];
     
     public function getField(string $key): array
     {
@@ -13,9 +17,7 @@ class IntimAnketaContract {
             'info' => (new InfoContract($key))->getData(),
             'service' => (new ServiceContract())->getData(),
             'price' => (new PriceContract($key))->getData(),
-            'dop' => [
-                'text' => ['name' => 'Дополнительно о себе', 'type' => 'textarea', 'require' => 1]
-            ]
+            'dop' => self::FIELD_DOP
         ];
     }
 
